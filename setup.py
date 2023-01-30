c1adfaf068b50045df22084841af0d3cb3608f0e
Julio Alarcon Caballero <tazkech@gmail.com>
2023-01-30 02:58:01 -0300

    python clientes v2

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name = "paquete_proyecto_2",
-    version = "1.6",
+    name = "segunda_pre-entrega_alarcon",
+    version = "1.8",
     description = "Paquete distribuido para el proyecto de CoderHouse",
     author = "Julio Alarcon Caballero",
     author_email = "julio.alarconca@gmail.com",
-    packages = find_packages(exclude=['test']),
+    packages = find_packages(["segunda_pre-entrega_alarcon"]),
 )
